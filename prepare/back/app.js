a490f05bc5f957baf442c928213f00016d5eb2a3
JMinkyoung <whk5817@gmail.com>
2022-03-15 21:51:29 +0900

    CORS 설정

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -2,9 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
 const { PORT, DB_PASS } = process.env;
 const mainRouter = require("./routes/routes");
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
 mongoose
     .connect(`mongodb+srv://admin:${DB_PASS}@recipecluster.zsgvx.mongodb.net/main?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('MongoDB connected...'))
