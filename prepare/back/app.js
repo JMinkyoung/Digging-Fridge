32ff8c28fbcf9ed119b9328217143994a3ab1d41
JMinkyoung <whk5817@gmail.com>
2022-03-21 21:43:32 +0900

    SameSite Error Fix

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+var cookie = require('cookie');
 const app = express();
 const cors = require('cors');
 const { PORT, DB_PASS, M_URL } = process.env;
@@ -10,6 +11,9 @@ app.use(cors({
     origin: ['http://localhost:3000',`http://${M_URL}:3000`],
     credentials: true,
 }));
+
+cookie.setCookie = (res, token) => { res.cookie("access_token", token, { sameSite:'none', secure: true, }); }
+
 mongoose
     .connect(`mongodb+srv://admin:${DB_PASS}@recipecluster.zsgvx.mongodb.net/main?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('MongoDB connected...'))
