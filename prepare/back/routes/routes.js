0e8922dcf4d729cce50f17d59ba9192bdae5a92f
JMinkyoung <whk5817@gmail.com>
2022-03-22 21:44:10 +0900

    Input Tag 검색 Router

--- prepare/back/routes/routes.js
+++ prepare/back/routes/routes.js
@@ -2,13 +2,27 @@ const express = require("express");
 const router = express.Router();
 const Main = require("../models/recipe");
 
-router.get("/", async (req, res) => {   //  GET /recipes?lastId=
+router.get("/", async (req, res) => {   //  GET /recipes?lastId= || /recipes
   try{    
     let lastId = req.query.lastId;
     let query = {};
     if(lastId) query =  {'_id' : { "$gt" : lastId}};
     const recipes = await Main.find(query).limit(5);
     // const recipes=await Main.find( { $and: [{"ingredientKey": { $regex: "스팸"} },{"ingredientKey": { $regex: "김치"} },{"ingredientKey": { $regex: "파"} } ] } );
+    res.status(200).json(recipes);
+  }catch(err){
+    res.status(500).json({message: err.message});
+  }
+});
+
+router.get("/tags/*", async (req, res) => {   //  GET /recipes/tags/tag1/tag2/tag3...
+  try{
+    const tags = req.params[0].split("/");
+    const query = { $and: [] };
+    tags.forEach((t)=>{
+      query.$and.push({"ingredientKey": { $regex: decodeURIComponent(t)}});
+    });
+    const recipes = await Main.find(query);
     res.status(200).json(recipes);
   }catch(err){
     res.status(500).json({message: err.message});
