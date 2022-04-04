ea750bb199f11c3e6624841488eb2ccc82f5fb9d
JMinkyoung <whk5817@gmail.com>
2022-04-04 19:22:44 +0900

    배포 관련 수정

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -8,7 +8,7 @@ const { PORT, DB_PASS, M_URL } = process.env;
 const mainRouter = require("./routes/routes");
 
 app.use(cors({
-    origin: ['http://localhost:3000',`http://${M_URL}:3000`],
+    origin: ['http://localhost:3000',`http://${M_URL}:3000`, 'http://34.238.85.230'],
     credentials: true,
 }));
 
